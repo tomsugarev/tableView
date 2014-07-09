c6c4cf7b0252babe8fcaa22b482982bc7d9bc551
kodah <tom_1440@hotmail.co.uk>
2014-07-09 01:32:21 +0100

    init

---
+++ Body fat calculator/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  Body fat calculator
+//
+//  Created by Tom on 08/07/2014.
+//  Copyright (c) 2014 SugWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
